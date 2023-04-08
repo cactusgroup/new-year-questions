614285d7fafaff27391c4d14412005bb6f76f123
unknown <joshuachu007@gmail.com>
2023-04-08 10:06:33 -0400

    update years and stylesheet

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 document.addEventListener("DOMContentLoaded", function (event) {
     // get and fill span#year with current year
-    const year = new Date().toDateString().split(" ")[3];
+    const year = Number(new Date().toDateString().split(" ")[3]);
     const years = document.getElementsByClassName("year");
-    for (let i = 0; i < years.length; i++) {
-        years[i].innerHTML = year;
-    }
+    // loop unrolling; known in advance that there are 2 occurrences
+    years[0].innerHTML = "" + year;
+    years[1].innerHTML = "" + (year - 1);
     let printPage = document.getElementById("print");
     printPage.addEventListener("click", function (event) {
-        console.log("printing");
         window.print();
     });
 });
